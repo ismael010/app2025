2d10158aa8f74ab30e568b28585b00131e73cd2f
ismael010 <ihonoratoa@udd.cl>
2025-08-28 19:09:57 -0400

    Primera version de la app

---
+++ appuno/android/app/src/main/kotlin/com/example/appuno/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.appuno
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ appuno/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: const Icon(Icons.menu),
+          title: const Text('Diagramación'),
+          actions: const [
+            Icon(Icons.more_vert),
+          ],
+        ),
+        body: ListView(
+          children: [
+            ListTile(
+              leading: const CircleAvatar(
+                backgroundColor: Colors.blue,
+              ),
+              title: const Text('Item 1'),
+              trailing: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Acción'),
+              ),
+            ),
+            const Divider(),
+            ListTile(
+              leading: const CircleAvatar(
+                backgroundColor: Colors.green,
+              ),
+              title: const Text('Item 2'),
+              trailing: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Acción'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
